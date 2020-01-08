b5ee0e5baef47846925a07de4b09998c4965833b
Gustaf Lindholm <babiluskus@gmail.com>
2020-01-08 20:23:34 +0100

    installed react-helmet-async

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import Layout from "./components/Layout/Layout";
 import { BrowserRouter, Route } from "react-router-dom";
 import { AppLanguage, AppRoute } from "./const";
-import Home from "./containers/Home/Home"
-import Cafe from "./containers/Cafe/Cafe"
-import Error from "./components/Error"
+import { HelmetProvider } from "react-helmet-async";
+import Home from "./containers/Home/Home";
+import Cafe from "./containers/Cafe/Cafe";
+import Error from "./components/Error";
 import {
   LocalizedRouter,
   LocalizedSwitch,
@@ -14,23 +15,32 @@ import Bakery from "./components/Bakery/Bakery";
 import Products from "./components/Products/Products";
 
 function App() {
-
   return (
-    <LocalizedRouter
-      RouterComponent={BrowserRouter}
-      languages={AppLanguage}
-      appStrings={appStrings}
-    >
-      <Layout>
-        <LocalizedSwitch>
-          <Route exact path={AppRoute.Home}>{Home}</Route>
-          <Route exact path={AppRoute.Cafe}>{Cafe}</Route>
-          <Route exact path={AppRoute.Bakery}>{Bakery}</Route>
-          <Route exact path={AppRoute.Products}>{Products}</Route>
-          <Route path="*">{Error}</Route>
-        </LocalizedSwitch>
-      </Layout>
-    </LocalizedRouter>
+    <HelmetProvider>
+      <LocalizedRouter
+        RouterComponent={BrowserRouter}
+        languages={AppLanguage}
+        appStrings={appStrings}
+      >
+        <Layout>
+          <LocalizedSwitch>
+            <Route exact path={AppRoute.Home}>
+              {Home}
+            </Route>
+            <Route exact path={AppRoute.Cafe}>
+              {Cafe}
+            </Route>
+            <Route exact path={AppRoute.Bakery}>
+              {Bakery}
+            </Route>
+            <Route exact path={AppRoute.Products}>
+              {Products}
+            </Route>
+            <Route path="*">{Error}</Route>
+          </LocalizedSwitch>
+        </Layout>
+      </LocalizedRouter>
+    </HelmetProvider>
   );
 }
 
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,12 +1,21 @@
 import React from "react";
-
+import { Helmet } from "react-helmet-async";
 export default function Home(props) {
   const background = {
     backgroundImage: "url(https://dummyimage.com/1000x1000.jpg)"
   };
   return (
-    <article className="bg-white">
-      <div className="vh-75 cover bg-center" style={background}></div>
-    </article>
+    <>
+      <Helmet>
+        <title>Backers Baker - Ekologiskt bröd</title>
+      </Helmet>
+      <article className="bg-white">
+        <div className="vh-25 cover bg-center" style={background}></div>
+        <div className="fl w-100 w-25-ns pa2">1</div>
+        <div className="fl w-100 w-25-ns pa2">2</div>
+        <div className="fl w-100 w-25-ns pa2">3</div>
+        <div className="fl w-100 w-25-ns pa2">4</div>
+      </article>
+    </>
   );
 }
